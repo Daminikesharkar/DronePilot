08730746862ff72f9f47367e56b20a98c9cbcbac
Daminikesharkar <kesharkardamini1234@gmail.com>
2024-12-20 01:46:29 +0530

    Added authentication middleware for token verification and user validation

---
+++ src/middleware/authenticate.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import User from '../models/user';
+
+declare global {
+    namespace Express {
+      interface Request {
+        user?: any; 
+      }
+    }
+}
+
+export const authenticate = async(req:Request,res:Response,next:NextFunction)=>{
+    try {
+        const token = req.header('Autherization');
+        if(token){
+            const user = jwt.verify(token,process.env.SECRETKEY as string) as JwtPayload;
+            const foundUser = await User.findById(user.userId);
+
+            if(user){
+                req.user = foundUser;
+                next();
+            } else {
+                res.status(401).json({
+                    message: 'Unauthorized'
+                });
+            }          
+        } else {
+            res.status(400).json({
+                message: 'No token provided'
+            });          
+        }      
+        
+    } catch (error) {
+        res.status(500).json({
+            error: 'Internal Server Error'
+        });
+    }
+}
